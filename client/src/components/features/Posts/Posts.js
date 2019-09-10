e393f7e7ae72db8dfc195c19ecd6ea4f527d3575
Paweł Żurawka <pzurawka@gmail.com>
2019-09-10 14:44:54 +0200

    Finish task 1

--- client/src/components/features/Posts/Posts.js
+++ client/src/components/features/Posts/Posts.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import PostsList from '../PostsList/PostsList';
 import Spinner from '../../common/Spinner/Spinner';
+import Alert from '../../common/Alert/Alert';
 
 class Posts extends React.Component {
   componentDidMount() {
@@ -12,12 +13,45 @@ class Posts extends React.Component {
   render() {
     const { posts, request } = this.props;
 
-    return (
-      <div>
-        {request.pending && <Spinner />}
-        <PostsList posts={posts} />
-      </div>
-    );
+    if (
+      request.pending === false &&
+      request.success === true &&
+      posts.length > 0
+    ) {
+      return (
+        <div>
+          <PostsList posts={posts} />
+        </div>
+      );
+    } else if (request.pending === true || request.success === null) {
+      return (
+        <div>
+          <Spinner />
+        </div>
+      );
+    } else if (request.pending === false && request.error != null) {
+      return (
+        <div>
+          <Alert variant='error'>{request.error}</Alert>
+        </div>
+      );
+    } else if (
+      request.pending === false &&
+      request.success === true &&
+      posts.length === 0
+    ) {
+      return (
+        <div>
+          <Alert variant='info'>No posts</Alert>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Alert variant='info'>Something went wrong!</Alert>
+        </div>
+      );
+    }
   }
 }
 
