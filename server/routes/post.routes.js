be7f1081c5af712e1f607b0979ddc07d3d6b820d
Paweł Żurawka <pzurawka@gmail.com>
2019-09-13 12:47:59 +0200

    Finish task 30.7

--- server/routes/post.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const PostController = require('../controllers/post.controller');
-
-// get all posts
-router.route('/posts').get(PostController.getPosts);
-
-module.exports = router;
